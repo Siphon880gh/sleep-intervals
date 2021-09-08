ec0837b0ebc1efeaf7e84ae6a7b51b005022760f
Weng Fei Fung <weffung@ucdavis.edu>
2021-09-08 14:50:05 -0700

    Underlying data structures and flow

---
+++ assets/js/app.js
@@ -0,0 +1,67 @@
+/**
+ * 
+ * Settings is a global state with setting helpers.
+ * - The values are default and meant to be overridden if other saved settings found in localStorage
+ * - lastUpdated tracks the Unix time last saved, which helps the browser decide whether to re-render.
+ * - Never modify the values in [0] directly. Use save() method so that lastUpdated can refresh.
+ * 
+ */
+window.settings = {
+    lastUpdated: 0,
+    save: function() {
+        this.lastUpdated = Date.now();
+    },
+    0: {
+        timeWakeUpBy: "0600",
+        durationSleepToWake: 8,
+        steps: {
+            first: "Begin",
+            optional: [],
+            last: "Fall asleep",
+        }
+    }
+}
+
+/**
+ * Utility functions
+ */
+window.utility = {
+    cvtMilitaryTimeToFractional: (militaryTime) => {
+        // TODO.
+        return militaryTime;
+    },
+    cvtFractionalToMilitaryTime: (fractionalTime) => {
+        // TODO.
+        return fractionalTime;
+    },
+    countOptionalSteps: () => {
+        // TODO.
+        return 0;
+    }
+}
+
+/**
+ * Poller re-renders only when necessary to save performance
+ */
+class Poller {
+    #lastUpdatedSeen;
+    constructor(ms) {
+        this.#lastUpdatedSeen = -1;
+        setInterval(()=>{
+            if(this.#lastUpdatedSeen !== window.settings.lastUpdated) {
+                // TODO: Update lastUpdatedSeen to settings.lastUpdated then re-render the page
+                // TODO: Re-render includes looking at the Experimental section if it has an user input
+            }
+        }, ms);
+    }
+}
+
+
+$(()=>{
+    // Init poller
+    let poller = new Poller(100);
+
+    // Event listeners
+    // TODO.
+    // TODO: .focus-recommendation will toggle display on all other recommendations
+})
