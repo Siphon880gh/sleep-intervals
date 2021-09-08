401442f3661c0edb528cfe2bf85499552a263c2b
Weng Fei Fung <weffung@ucdavis.edu>
2021-09-08 14:55:11 -0700

    Preliminary test driven

---
+++ _test/app.js
@@ -0,0 +1,11 @@
+/**
+ * @file _test/app.js
+ * Emulate user behaviors then check DOM and model states
+ * This is a quick test using the browser's console.assert
+ * Does not break or count passes/fails.
+ * 
+ */
+
+$(()=>{
+    
+})
